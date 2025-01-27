aeeb885c44d16dfcc3457112bbbc618a96779eb7
Denis <denchickuss@yandex.ru>
2025-01-27 21:31:01 +0300

    Добавлена функция add_numbers для сложения чисел

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 def say_hello():
     print("Hello, World!")
 
+def add_numbers(a, b):
+    return a + b
+
+
 
 if __name__ == "__main__":
     say_hello()
+    print(f"4 + 5 = {add_numbers(4, 5)}")
